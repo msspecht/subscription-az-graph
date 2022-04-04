307ba1187a67b49cd51d52d3b8c8cff7dd4e13f8
Mario Specht <mario.araujo@pucrs.br>
2022-04-04 14:26:30 -0300

    Valida body do webhook

--- src/main/java/com/br/subscriptionazgraph/web/api/SubscriptionController.java
+++ src/main/java/com/br/subscriptionazgraph/web/api/SubscriptionController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/subscriptions", produces = "text/plain")
@@ -18,6 +19,8 @@ public class SubscriptionController {
 
     @PostMapping
     public ResponseEntity<?> responseSubscription(@RequestParam("validationToken") String validationToken, @RequestBody String body) {
+        if(Objects.isNull(body))
+            body = "Executou o endpoint";
         hookDeckFeign.createWebHook(URI.create("https://events.hookdeck.com/e/src_c3cD8mFCRldm8ein0YSMCCfO"), body);
         return new ResponseEntity<>(validationToken, HttpStatus.OK);
     }
